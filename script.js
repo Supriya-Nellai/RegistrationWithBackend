745b6dd3b3c697e1b020f9923dc7d64d690086f3
Supriya-Nellai <supriyanellaiyappan@gmail.com>
2026-01-06 13:44:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://registrationwithbackend.onrender.com/api/users"; 
+const API_BASE_URL = "https://registrationwithbackend-ika4.onrender.com"; 
 const openRegisterBtn = document.getElementById("open-register");
 const registerPopup = document.getElementById("register-popup");
 const closeBtn = document.getElementById("close");
@@ -257,4 +257,5 @@ if (modal) {
 }
 if (document.getElementById("usersBody")) {
     window.addEventListener("DOMContentLoaded", fetchUsers);
+
 }
